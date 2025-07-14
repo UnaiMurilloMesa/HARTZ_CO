a706af90fa857cf67024defc95332725c35189c8
UnaiMurilloMesa <unaimurillo123@gmail.com>
2025-07-14 17:50:16 +0200

    Creacion back

---
+++ hartz_backend/src/main/java/org/hartz/hartz_backend/HartzBackendApplication.java
@@ -0,0 +1,13 @@
+package org.hartz.hartz_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HartzBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HartzBackendApplication.class, args);
+    }
+
+}
---
+++ hartz_backend/src/test/java/org/hartz/hartz_backend/HartzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hartz.hartz_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HartzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
