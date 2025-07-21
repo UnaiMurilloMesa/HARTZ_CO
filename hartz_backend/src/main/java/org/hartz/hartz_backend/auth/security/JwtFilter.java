0cb6c22cbfa568c2627c3ebb8c875aab716ca963
UnaiMurilloMesa <unaimurillo123@gmail.com>
2025-07-21 23:08:52 +0200

    Arreglo error en docker-compose.yml con los volumenes

--- hartz_backend/src/main/java/org/hartz/hartz_backend/auth/security/JwtFilter.java
+++ hartz_backend/src/main/java/org/hartz/hartz_backend/auth/security/JwtFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.hartz.hartz_backend.auth.service.CustomUserDetailsService;
 import org.hartz.hartz_backend.auth.service.JwtService;
-import org.hartz.hartz_backend.user.persistence.UserRepository;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
