6f4488b84968a15994f27933dcff02775218f3bb
Alberto <amozosk@gmail.com>
2025-07-24 22:58:43 +0200

    Eliminada conf. no necesaria

--- hartz_backend/src/main/java/org/hartz/hartz_backend/config/SecurityConfig.java
+++ hartz_backend/src/main/java/org/hartz/hartz_backend/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
     @Order(1)
     public SecurityFilterChain openEndpoints(HttpSecurity http) throws Exception {
         return http
-                .securityMatcher("/api/auth/**", "/import/**") // Temporal hasta haber pasado el ETL de exercise, eliminar /import despues
+                .securityMatcher("/api/auth/**")
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
                 .csrf(csrf -> csrf.disable())
                 .build();
