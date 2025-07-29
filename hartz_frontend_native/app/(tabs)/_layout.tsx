49a10fdc8fafaa02d1b861659363fd5f5a0667b8
Alberto Mozos <amozosk@gmail.com>
2025-07-29 13:13:43 +0200

    Eliminada la cabecera

--- hartz_frontend_native/app/(tabs)/_layout.tsx
+++ hartz_frontend_native/app/(tabs)/_layout.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
 
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/utils/useColorScheme';
-
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
@@ -14,14 +11,11 @@ function TabBarIcon(props: {
 }
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
+        headerShown: false,
       }}>
       <Tabs.Screen
         name="index"
